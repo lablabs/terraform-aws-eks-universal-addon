7f6906cb72e2a3bfce555e68901284a82ea0c585
Balsir <jubo.stehlik@gmail.com>
2024-12-05 12:48:10 +0100

    feat(oidc): Add OIDC provider condition

--- variables-addon-oidc.tf
+++ variables-addon-oidc.tf
@@ -71,7 +71,7 @@ variable "oidc_tags" {
 variable "oidc_assume_role_policy_condition_test" {
   type        = string
   default     = null
-  description = "Specifies the condition test to use for the assume role trust policy. Defaults to `StringEquals`."
+  description = "Specifies the condition test to use for the assume role trust policy. Defaults to `StringLike`."
 }
 
 variable "oidc_assume_role_policy_condition_values" {
