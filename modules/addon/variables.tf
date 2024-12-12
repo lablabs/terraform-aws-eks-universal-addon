d91d2a739e0ef08017e903fad476855fed6a0a2a
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2024-12-12 09:27:57 +0100

    feat(addon): add helm release install control variable (#7)

--- modules/addon/variables.tf
+++ modules/addon/variables.tf
@@ -6,6 +6,12 @@ variable "enabled" {
 
 # ================ common variables (required) ================
 
+variable "helm_enabled" {
+  type        = bool
+  default     = null
+  description = "Set to false to prevent installation of the module via Helm release. Defaults to `true`."
+}
+
 variable "helm_chart_name" {
   type        = string
   default     = null
