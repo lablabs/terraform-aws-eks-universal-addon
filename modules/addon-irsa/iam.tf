ef9a7274aa4153c299a1f049be0d07c7de7ff694
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2025-01-10 16:28:26 +0100

    feat(modules): set variable defaults (#8)
    * feat(modules): set variable defaults
    
    * chore(terraform): remove lock files for addon modules

--- modules/addon-irsa/iam.tf
+++ modules/addon-irsa/iam.tf
@@ -1,13 +1,11 @@
 locals {
-  irsa_role_create         = var.enabled == true && var.rbac_create == true && var.service_account_create == true && var.irsa_role_create == true
-  irsa_role_name_prefix    = try(coalesce(var.irsa_role_name_prefix), "")
-  irsa_role_name           = try(trim("${local.irsa_role_name_prefix}-${var.irsa_role_name}", "-"), "")
-  irsa_policy_enabled      = var.irsa_policy_enabled == true && try(length(var.irsa_policy) > 0, false)
-  irsa_assume_role_enabled = var.irsa_assume_role_enabled == true && try(length(var.irsa_assume_role_arns) > 0, false)
-
-  irsa_assume_role_policy_condition_values_default = [
-    format("system:serviceaccount:%s:%s", var.service_account_namespace != null ? var.service_account_namespace : "", var.service_account_name != null ? var.service_account_name : "")
-  ]
+  irsa_role_create         = var.enabled && var.rbac_create && var.service_account_create && var.irsa_role_create
+  irsa_role_name           = trim("${var.irsa_role_name_prefix}-${var.irsa_role_name}", "-")
+  irsa_policy_enabled      = var.irsa_policy_enabled && length(var.irsa_policy) > 0
+  irsa_assume_role_enabled = var.irsa_assume_role_enabled && length(var.irsa_assume_role_arns) > 0
+  irsa_assume_role_policy_condition_values_default = length(var.service_account_namespace) > 0 && length(var.service_account_name) > 0 ? [
+    format("system:serviceaccount:%s:%s", var.service_account_namespace, var.service_account_name)
+  ] : [] # we want to use the default values only if the Service Account Namespace and name are defined
 }
 
 data "aws_iam_policy_document" "this_assume" {
@@ -47,22 +45,24 @@ data "aws_iam_policy_document" "this_irsa" {
     condition {
       test     = var.irsa_assume_role_policy_condition_test
       variable = "${replace(var.cluster_identity_oidc_issuer, "https://", "")}:sub"
-
-      values = coalescelist(var.irsa_assume_role_policy_condition_values, local.irsa_assume_role_policy_condition_values_default)
+      values   = coalescelist(var.irsa_assume_role_policy_condition_values, local.irsa_assume_role_policy_condition_values_default)
     }
   }
 }
 
 resource "aws_iam_role" "this" {
-  count                = local.irsa_role_create ? 1 : 0
+  count = local.irsa_role_create ? 1 : 0
+
   name                 = local.irsa_role_name # tflint-ignore: aws_iam_role_invalid_name
   assume_role_policy   = data.aws_iam_policy_document.this_irsa[0].json
   permissions_boundary = var.irsa_permissions_boundary
-  tags                 = var.irsa_tags
+
+  tags = var.irsa_tags
 }
 
 resource "aws_iam_role_policy_attachment" "this" {
-  count      = local.irsa_role_create && (local.irsa_policy_enabled || local.irsa_assume_role_enabled) ? 1 : 0
+  count = local.irsa_role_create && (local.irsa_policy_enabled || local.irsa_assume_role_enabled) ? 1 : 0
+
   role       = aws_iam_role.this[0].name
   policy_arn = aws_iam_policy.this[0].arn
 }
