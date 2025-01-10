5a6da77adbdfef56e26c0c87748872172d56ec0e
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2025-01-10 14:35:40 +0100

    feat(modules): set variable defaults

--- modules/addon-irsa/variables.tf
+++ modules/addon-irsa/variables.tf
@@ -1,111 +1,107 @@
 variable "enabled" {
   type        = bool
   default     = true
-  description = "Set to false to prevent the module from creating any resources"
+  description = "Set to false to prevent the module from creating any resources."
 }
-
-# ================ IRSA variables (optional) ================
 
 variable "cluster_identity_oidc_issuer" {
   type        = string
-  default     = null
   description = "The OIDC Identity issuer for the cluster (required)."
 }
 
 variable "cluster_identity_oidc_issuer_arn" {
   type        = string
-  default     = null
   description = "The OIDC Identity issuer ARN for the cluster that can be used to associate IAM roles with a Service Account (required)."
 }
 
 variable "rbac_create" {
   type        = bool
-  default     = null
-  description = "Whether to create and use RBAC resources. Defaults to `true`."
+  default     = true
+  description = "Whether to create and use RBAC resources."
 }
 
 variable "service_account_create" {
   type        = bool
-  default     = null
-  description = "Whether to create Service Account. Defaults to `true`."
+  default     = true
+  description = "Whether to create Service Account."
 }
 
 variable "service_account_name" {
   type        = string
-  default     = null
-  description = "The Kubernetes Service Account name. Defaults to addon name."
+  default     = ""
+  description = "The Kubernetes Service Account name. Defaults to the addon name."
 }
 
 variable "service_account_namespace" {
   type        = string
-  default     = null
-  description = "The Kubernetes Service Account namespace. Defaults to addon namespace."
+  default     = ""
+  description = "The Kubernetes Service Account namespace. Defaults to the addon namespace."
 }
 
 variable "irsa_role_create" {
   type        = bool
-  default     = null
-  description = "Whether to create IRSA role and annotate Service Account. Defaults to `true`."
+  default     = true
+  description = "Whether to create IRSA role and annotate Service Account."
 }
 
 variable "irsa_role_name_prefix" {
   type        = string
-  default     = null
-  description = "IRSA role name prefix. Defaults to addon IRSA component name with `irsa` suffix."
+  default     = ""
+  description = "IRSA role name prefix. Either `irsa_role_name_prefix` or `irsa_role_name` must be set."
 }
 
 variable "irsa_role_name" {
   type        = string
-  default     = null
-  description = "IRSA role name. The value is prefixed by `var.irsa_role_name_prefix`. Defaults to addon Helm chart name."
+  default     = ""
+  description = "IRSA role name. The value is prefixed by `irsa_role_name_prefix`. Either `irsa_role_name` or `irsa_role_name_prefix` must be set."
 }
 
 variable "irsa_policy_enabled" {
   type        = bool
-  default     = null
-  description = "Whether to create IAM policy specified by `irsa_policy`. Mutually exclusive with `irsa_assume_role_enabled`. Defaults to `false`."
+  default     = false
+  description = "Whether to create IAM policy specified by `irsa_policy`. Mutually exclusive with `irsa_assume_role_enabled`."
 }
 
 variable "irsa_policy" {
   type        = string
-  default     = null
-  description = "Policy to be attached to the IRSA role. Applied only if `irsa_policy_enabled` is `true`."
+  default     = ""
+  description = "AWS IAM policy JSON document to be attached to the IRSA role. Applied only if `irsa_policy_enabled` is `true`."
 }
 
 variable "irsa_assume_role_enabled" {
   type        = bool
-  default     = null
-  description = "Whether IRSA is allowed to assume role defined by `irsa_assume_role_arn`. Mutually exclusive with `irsa_policy_enabled`. Defaults to `false`."
+  default     = false
+  description = "Whether IRSA is allowed to assume role defined by `irsa_assume_role_arn`. Mutually exclusive with `irsa_policy_enabled`."
 }
 
 variable "irsa_assume_role_arns" {
   type        = list(string)
-  default     = null
-  description = "List of ARNs assumable by the IRSA role. Applied only if `irsa_assume_role_enabled` is `true`. Defaults to `[]`."
+  default     = []
+  description = "List of ARNs assumable by the IRSA role. Applied only if `irsa_assume_role_enabled` is `true`."
 }
 
 variable "irsa_permissions_boundary" {
   type        = string
   default     = null
-  description = "ARN of the policy that is used to set the permissions boundary for the IRSA role. Defaults to `\"\"`."
+  description = "ARN of the policy that is used to set the permissions boundary for the IRSA role."
 }
 
 variable "irsa_additional_policies" {
   type        = map(string)
-  default     = null
-  description = "Map of the additional policies to be attached to IRSA role. Where key is arbitrary id and value is policy ARN. Defaults to `{}`."
+  default     = {}
+  description = "Map of the additional policies to be attached to IRSA role. Where key is arbitrary id and value is policy ARN."
 }
 
 variable "irsa_tags" {
   type        = map(string)
-  default     = null
-  description = "IRSA resources tags. Defaults to `{}`."
+  default     = {}
+  description = "IRSA resources tags."
 }
 
 variable "irsa_assume_role_policy_condition_test" {
   type        = string
   default     = "StringEquals"
-  description = "Specifies the condition test to use for the assume role trust policy. Defaults to `StringEquals`."
+  description = "Specifies the condition test to use for the assume role trust policy."
 }
 
 variable "irsa_assume_role_policy_condition_values" {
--- modules/addon-oidc/variables.tf
+++ modules/addon-oidc/variables.tf
@@ -4,112 +4,110 @@ variable "enabled" {
   description = "Set to false to prevent the module from creating any resources"
 }
 
-# ================ oidc variables (optional) ================
-
 variable "oidc_provider_create" {
   type        = bool
-  default     = null
-  description = "Whether to create OIDC provider. Defaults to `true`. Set to false if you want to disable default OIDC provider when oidc_custom_provider_arn is set."
+  default     = true
+  description = "Whether to create OIDC provider. Set to `false` if you want to disable default OIDC provider when `oidc_custom_provider_arn` is set."
+}
+
+variable "oidc_custom_provider_arn" {
+  type        = string
+  default     = ""
+  description = "Specifies a custom OIDC provider ARN."
 }
 
 variable "oidc_role_create" {
   type        = bool
-  default     = null
-  description = "Whether to create OIDC role and annotate Service Account. Defaults to `true`."
+  default     = true
+  description = "Whether to create OIDC role."
 }
 
 variable "oidc_role_name_prefix" {
   type        = string
-  default     = null
-  description = "OIDC role name prefix. Defaults to addon OIDC component name with `oidc` suffix."
+  default     = ""
+  description = "OIDC role name prefix. Either `oidc_role_name_prefix` or `oidc_role_name` must be set."
 }
 
 variable "oidc_role_name" {
   type        = string
-  default     = null
-  description = "OIDC role name. The value is prefixed by `var.oidc_role_name_prefix`. Defaults to addon Helm chart name."
+  default     = ""
+  description = "OIDC role name. The value is prefixed by `oidc_role_name_prefix`. Either `oidc_role_name` or `oidc_role_name_prefix` must be set."
 }
 
 variable "oidc_policy_enabled" {
   type        = bool
-  default     = null
-  description = "Whether to create IAM policy specified by `oidc_policy`. Mutually exclusive with `oidc_assume_role_enabled`. Defaults to `false`."
+  default     = false
+  description = "Whether to create IAM policy specified by `oidc_policy`. Mutually exclusive with `oidc_assume_role_enabled`."
 }
 
 variable "oidc_policy" {
   type        = string
-  default     = null
-  description = "Policy to be attached to the OIDC role. Applied only if `oidc_policy_enabled` is `true`."
+  default     = ""
+  description = "AWS IAM policy JSON document to be attached to the OIDC role. Applied only if `oidc_policy_enabled` is `true`."
 }
 
 variable "oidc_assume_role_enabled" {
   type        = bool
-  default     = null
-  description = "Whether OIDC is allowed to assume role defined by `oidc_assume_role_arn`. Mutually exclusive with `oidc_policy_enabled`. Defaults to `false`."
+  default     = false
+  description = "Whether OIDC is allowed to assume role defined by `oidc_assume_role_arn`. Mutually exclusive with `oidc_policy_enabled`."
 }
 
 variable "oidc_assume_role_arns" {
   type        = list(string)
-  default     = null
-  description = "List of ARNs assumable by the OIDC role. Applied only if `oidc_assume_role_enabled` is `true`. Defaults to `[]`."
+  default     = []
+  description = "List of ARNs assumable by the OIDC role. Applied only if `oidc_assume_role_enabled` is `true`."
 }
 
 variable "oidc_permissions_boundary" {
   type        = string
   default     = null
-  description = "ARN of the policy that is used to set the permissions boundary for the OIDC role. Defaults to `\"\"`."
+  description = "ARN of the policy that is used to set the permissions boundary for the OIDC role."
 }
 
 variable "oidc_additional_policies" {
   type        = map(string)
-  default     = null
-  description = "Map of the additional policies to be attached to OIDC role. Where key is arbitrary id and value is policy ARN. Defaults to `{}`."
+  default     = {}
+  description = "Map of the additional policies to be attached to OIDC role. Where key is arbitrary id and value is policy ARN."
 }
 
 variable "oidc_tags" {
   type        = map(string)
-  default     = null
-  description = "OIDC resources tags. Defaults to `{}`."
+  default     = {}
+  description = "OIDC resources tags."
 }
 
 variable "oidc_assume_role_policy_condition_test" {
   type        = string
   default     = "StringLike"
-  description = "Specifies the condition test to use for the assume role trust policy. Defaults to `StringLike`."
+  description = "Specifies the condition test to use for the assume role trust policy."
 }
 
 variable "oidc_assume_role_policy_condition_values" {
   type        = list(string)
   default     = []
-  description = "Specifies the values for the assume role trust policy condition. Defaults to `[]`."
+  description = "Specifies the values for the assume role trust policy condition."
 }
 
 variable "oidc_assume_role_policy_condition_variable" {
   type        = string
-  default     = null
-  description = "Specifies the variable to use for the assume role trust policy. Defaults to `\"\"`."
+  default     = ""
+  description = "Specifies the variable to use for the assume role trust policy."
 }
 
 variable "oidc_openid_client_ids" {
   type        = list(string)
   default     = []
-  description = "List of OpenID Connect client IDs that are allowed to assume the OIDC provider. Defaults to `[]`."
+  description = "List of OpenID Connect client IDs that are allowed to assume the OIDC provider."
 }
 
 variable "oidc_openid_provider_url" {
   type        = string
-  default     = null
-  description = "OIDC provider URL. Defaults to `\"\"`."
+  default     = ""
+  description = "OIDC provider URL."
 }
 
 variable "oidc_openid_thumbprints" {
   type        = list(string)
   default     = []
-  description = "List of thumbprints of the OIDC provider's server certificate. Defaults to `[]`."
-}
-
-variable "oidc_custom_provider_arn" {
-  type        = string
-  default     = null
-  description = "Specifies a custom OIDC provider ARN. If specified, overrides provider created by this module. If set, it is recommended to disable default OIDC provider creation by setting var.oidc_provider_create to false. Defaults to `\"\"`."
+  description = "List of thumbprints of the OIDC provider's server certificate."
 }
