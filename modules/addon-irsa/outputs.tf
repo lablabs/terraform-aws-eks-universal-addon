4e0cfe5557b555f569dfc031dbc49bfe86449f59
Matej Hasul <matej.hasul@lablabs.io>
2025-05-06 11:50:39 +0200

    feat(addon): Add support for pod identity to IRSA module
    I've renamed `iam.tf` to `iam-irsa.tf` to keep conditions simple and to prepare for easy removal of IRSA when we decide to deprecate it. I had to rename resources (`this` -> `irsa`) which means we have to prepare also migration resources.

--- modules/addon-irsa/outputs.tf
+++ modules/addon-irsa/outputs.tf
@@ -3,6 +3,11 @@ output "irsa_role_enabled" {
   value       = local.irsa_role_create
 }
 
+output "pod_identity_role_enabled" {
+  description = "Whether pod identity role is enabled"
+  value       = local.pod_identity_role_create
+}
+
 output "iam_role_attributes" {
   description = "IAM role attributes"
   value       = try(aws_iam_role.this[0], {})
--- modules/addon-irsa/variables.tf
+++ modules/addon-irsa/variables.tf
@@ -109,3 +109,52 @@ variable "irsa_assume_role_policy_condition_values" {
   default     = []
   description = "Specifies the values for the assume role trust policy condition. Each entry in this list must follow the required format `system:serviceaccount:$service_account_namespace:$service_account_name`. If this variable is left as the default, `local.irsa_assume_role_policy_condition_values_default` is used instead, which is a list containing a single value. Note that if this list is defined, the `service_account_name` and `service_account_namespace` variables are ignored."
 }
+
+
+variable "cluster_name" {
+  type        = string
+  default     = ""
+  description = "The name of the cluster. This is mandatory when pod identity is enabled."
+}
+
+variable "pod_identity_role_create" {
+  type        = bool
+  default     = false
+  description = "Whether to create pod identity role and annotate Service Account."
+}
+
+variable "pod_identity_role_name_prefix" {
+  type        = string
+  default     = ""
+  description = "Pod identity role name prefix. Either `pod_identity_role_name_prefix` or `pod_identity_role_name` must be set."
+}
+
+variable "pod_identity_role_name" {
+  type        = string
+  default     = ""
+  description = "Pod identity role name. The value is prefixed by `pod_identity_role_name_prefix`. Either `pod_identity_role_name` or `pod_identity_role_name_prefix` must be set."
+}
+
+variable "pod_identity_policy_enabled" {
+  type        = bool
+  default     = false
+  description = "Whether to create IAM policy specified by `pod_identity_policy`. Mutually exclusive with `pod_identity_assume_role_enabled`."
+}
+
+variable "pod_identity_policy" {
+  type        = string
+  default     = ""
+  description = "AWS IAM policy JSON document to be attached to the pod identity role. Applied only if `pod_identity_policy_enabled` is `true`."
+}
+
+variable "pod_identity_permissions_boundary" {
+  type        = string
+  default     = null
+  description = "ARN of the policy that is used to set the permissions boundary for the pod identity role."
+}
+
+variable "pod_identity_additional_policies" {
+  type        = map(string)
+  default     = {}
+  description = "Map of the additional policies to be attached to pod identity role. Where key is arbitrary id and value is policy ARN."
+}
