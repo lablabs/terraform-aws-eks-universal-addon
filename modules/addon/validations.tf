40197491716dc2b9c784e9412eb9f321fa69ac2f
Attila Sovak <attila.sovak@gmail.com>
2025-05-28 18:53:51 +0200

    fix(universal-addon): split precondition

--- modules/addon/validations.tf
+++ modules/addon/validations.tf
@@ -3,7 +3,7 @@ resource "terraform_data" "validations" {
     precondition {
       condition = !local.argo_application_source_helm_enabled || (
         var.helm_repo_url != ""
-        && (var.helm_chart_name != "" || var.argo_source_path != "")
+        && var.helm_chart_name != ""
         && var.helm_chart_version != ""
         && var.helm_release_name != ""
       )
@@ -21,6 +21,16 @@ resource "terraform_data" "validations" {
         && var.argo_name != ""
       )
       error_message = "The `argo_source_repo_url`, `argo_source_path`, `argo_source_target_revision` and `argo_name` variables must be set when argo_source_type is set to `kustomize` or `directory`."
+    }
+
+    precondition {
+      condition = !local.argo_application_source_helm_directory_enabled || (
+        var.helm_repo_url != ""
+        && (var.helm_chart_name != "" || var.argo_source_path != "")
+        && var.helm_chart_version != ""
+        && var.helm_release_name != ""
+      )
+      error_message = "The `helm_repo_url`, `helm_chart_name` (or `argo_source_path`), `helm_chart_version`, and `helm_release_name` variables must be set when argo_source_type is set to `helm-directory`."
     }
   }
 }
