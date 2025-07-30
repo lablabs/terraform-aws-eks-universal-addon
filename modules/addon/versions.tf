c63c0ba5a17708d48a4dc3c01ddca5ff115eeab4
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2025-07-30 17:58:08 +0200

    fix(versions): relax contraints to allow upper versions

--- modules/addon/versions.tf
+++ modules/addon/versions.tf
@@ -1,22 +1,22 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.5"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5"
+      version = ">= 5"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.20"
+      version = ">= 2.20"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.6"
+      version = ">= 2.6"
     }
     utils = {
       source  = "cloudposse/utils"
-      version = "~> 1"
+      version = ">= 1"
     }
   }
 }
