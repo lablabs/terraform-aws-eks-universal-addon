4059ae3c582315a79a6f3f554777b2d1820fcc5a
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2026-01-14 14:52:22 +0100

    chore: update module source references to v0.0.25

--- addon-oidc.tf
+++ addon-oidc.tf
@@ -2,7 +2,7 @@
 module "addon-oidc" {
   for_each = local.addon_oidc
 
-  source = "git::https://github.com/lablabs/terraform-aws-eks-universal-addon.git//modules/addon-oidc?ref=v0.0.25-rc1"
+  source = "git::https://github.com/lablabs/terraform-aws-eks-universal-addon.git//modules/addon-oidc?ref=v0.0.25"
 
   enabled = var.enabled
 
