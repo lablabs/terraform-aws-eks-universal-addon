ad11cde023c8edaf57ad1a5eff2333fe636c6c34
mliner <matej.liner@lablabs.io>
2026-02-12 15:44:39 +0100

    testing

--- addon.tf
+++ addon.tf
@@ -11,7 +11,8 @@ locals {
 }
 
 module "addon" {
-  source = "git::https://github.com/lablabs/terraform-aws-eks-universal-addon.git//modules/addon?ref=v0.0.25"
+  # source = "git::https://github.com/lablabs/terraform-aws-eks-universal-addon.git//modules/addon?ref=v0.0.25"
+  source = "./modules/addon"
 
   enabled = var.enabled
 
